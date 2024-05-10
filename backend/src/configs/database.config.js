546515963f8fb2a6949cc7ae87d0cf95e880910e
Ervan Chodry <ervanchodry112@gmail.com>
2024-05-11 01:05:56 +0700

    create database config

---
+++ backend/src/configs/database.config.js
@@ -0,0 +1,11 @@
+
+const db_config = {
+   DB_DRIVER: process.env.DB_DRIVER,
+   DB_USER: process.env.DB_USER,
+   DB_HOST: process.env.DB_HOST,
+   DB_NAME: process.env.DB_NAME,
+   DB_PASSWORD: process.env.DB_PASSWORD,
+   DB_PORT: process.env.DB_PORT,
+}
+
+export default db_config;
